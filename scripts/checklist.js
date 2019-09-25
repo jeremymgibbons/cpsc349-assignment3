18cd8136e14b39a4b60bcdf035819e388d084cd9
jeremymgibbons <jeremymgibbons@gmail.com>
2019-09-24 19:04:56 -0700

    fixed eslint errors

--- scripts/checklist.js
+++ scripts/checklist.js
@@ -1,20 +1,20 @@
 (function(window) {
-  'use strict';
+  "use strict";
   var App = window.App || {};
   var $ = window.jQuery;
 
   function CheckList(selector) {
     if (!selector) {
-      throw new Error('No selector provided');
+      throw new Error("No selector provided");
     }
     this.$element = $(selector);
     if (this.$element.length === 0) {
-      throw new Error('Could not find element with selector: ' + selector);
+      throw new Error("Could not find element with selector: " + selector);
     }
   }
 
   CheckList.prototype.addClickHandler = function(fn) {
-    this.$element.on('click', 'input', function(event) {
+    this.$element.on("click", "input", function(event) {
       var email = event.target.value;
       this.removeRow(email);
       fn(email);
@@ -34,31 +34,31 @@
 
   CheckList.prototype.removeRow = function(email) {
     this.$element
-      .find('[value="' + email + '"]')
-      .closest('[data-coffee-order="checkbox"]')
+      .find("[value=\"" + email + "\"]")
+      .closest("[data-coffee-order=\"checkbox\"]")
       .remove();
   };
 
   function Row(coffeeOrder) {
-    var $div = $('<div></div>', {
-      'data-coffee-order': 'checkbox',
-      'class': 'checkbox'
+    var $div = $("<div></div>", {
+      "data-coffee-order": "checkbox",
+      "class": "checkbox"
     });
 
-    var $label = $('<label></label>');
-    var $checkbox = $('<input></input>', {
-      type: 'checkbox',
+    var $label = $("<label></label>");
+    var $checkbox = $("<input></input>", {
+      type: "checkbox",
       value: coffeeOrder.emailAddress
     });
 
-    var description = coffeeOrder.size + ' ';
+    var description = coffeeOrder.size + " ";
     if (coffeeOrder.flavor) {
-      description += coffeeOrder.flavor + ' ';
+      description += coffeeOrder.flavor + " ";
     }
 
-    description += coffeeOrder.coffee + ', ';
-    description += ' (' + coffeeOrder.emailAddress + ')';
-    description += ' [' + coffeeOrder.strength + 'x]';
+    description += coffeeOrder.coffee + ", ";
+    description += " (" + coffeeOrder.emailAddress + ")";
+    description += " [" + coffeeOrder.strength + "x]";
 
     $label.append($checkbox);
     $label.append(description);
